d06509fb9f813fb3a90f66accdcca0e9299c4262
phoomperfectionist <t.p.pungchaichan@gmail.com>
2020-07-04 17:35:25 +0700

    Updates

---
+++ precache-manifest.3481ea63d048f30b14e6572056575482.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64afeb4d1168994d07ddd131265a32b9",
+    "url": "/phoomperfectionist.github.io/index.html"
+  },
+  {
+    "revision": "f3b6121a3148760564b3",
+    "url": "/phoomperfectionist.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5702c017535aea2c3f49",
+    "url": "/phoomperfectionist.github.io/static/js/2.b26f6868.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/phoomperfectionist.github.io/static/js/2.b26f6868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3b6121a3148760564b3",
+    "url": "/phoomperfectionist.github.io/static/js/main.26e64700.chunk.js"
+  },
+  {
+    "revision": "ed5fbde33bddf18dd405",
+    "url": "/phoomperfectionist.github.io/static/js/runtime-main.f51d2e6c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/phoomperfectionist.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
