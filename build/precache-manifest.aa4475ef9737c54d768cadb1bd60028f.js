a3686e44f5572d306c03eeaf70e1beabcf214a0b
phoomperfectionist <t.p.pungchaichan@gmail.com>
2020-07-04 17:42:14 +0700

    create react app

---
+++ build/precache-manifest.aa4475ef9737c54d768cadb1bd60028f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccee92ed1feb8df35119c1ac81dd1233",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5689d19fb7713677609c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5702c017535aea2c3f49",
+    "url": "/static/js/2.b26f6868.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b26f6868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5689d19fb7713677609c",
+    "url": "/static/js/main.7f90987b.chunk.js"
+  },
+  {
+    "revision": "bb24ae6132aca2233907",
+    "url": "/static/js/runtime-main.b4987629.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
